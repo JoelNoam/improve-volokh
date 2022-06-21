3bdf65814806e24c6d825d655ae8ee12326120cc
JoelNoam <personal@joelnoam.com>
2022-06-21 15:35:01 -0500

    Update content.js
    try to have the core function run once on page load, and again when "MORE" is loaded. Only works for one reload, kind of hacky.

--- content.js
+++ content.js
@@ -1,15 +1,30 @@
 const replacementText = "Nope";
-var articles = document.getElementsByTagName('article');
 
-for (var i = 0; i < articles.length; i++) {
-  article = articles[i];
-  authorElement = article.getElementsByClassName("author-footer--author-name");
-  authorName = authorElement[0].textContent;
-  if (authorName === "Josh Blackman") {
-    var replacementElement = document.createElement('article');
-    var replacementContent = document.createTextNode(replacementText);
-    replacementElement.appendChild(replacementContent);
+window.onload = event => {
+  make_improvements();
+  var a = document.getElementsByClassName("rcom-load-more-button")[0]; // should just be one, I think
+  a.onclick = setTimeout(make_improvements, 6000); // waits some time for next part of page to load
+  // only works right now for the first load. Might have it run just every few seconds in an infinite loop
+};
 
-    article.replaceWith(replacementElement);
+function make_improvements() {
+  //window.alert("invoked");
+  console.log("invoked");
+  var articles = document.getElementsByTagName('article');
+  
+  for (var i = 0; i < articles.length; i++) {
+    article = articles[i];
+    authorElement = article.getElementsByClassName("author-footer--author-name");
+    authorName = authorElement[0].textContent;
+    console.log(authorName);
+    if (authorName === "Josh Blackman") {
+      //window.alert("found");
+      var replacementElement = document.createElement('h1');
+      var replacementContent = document.createTextNode(replacementText);
+      replacementElement.appendChild(replacementContent);
+      replacementElement.classList.add("rcom-standard-article"); // triggers green line formatting
+  
+      article.replaceWith(replacementElement);
+    }
   }
 }
