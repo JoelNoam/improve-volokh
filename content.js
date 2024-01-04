c5e848f6841f2ad646307e928a0de37b130bccee
JoelNoam <personal@joelnoam.com>
2024-01-04 16:09:58 -0600

    Update content.js
    Redo. Also, last commit made a bug.

--- content.js
+++ content.js
@@ -1,34 +1,42 @@
 const replacementText = "Nope";
 
 window.onload = event => {
-  make_improvements();
-  var a = document.getElementsByClassName("rcom-load-more-button")[0]; // should just be one, I think
-  a.onclick = setTimeout(make_improvements, 6000); // waits some time for next part of page to load
-  // only works right now for the first load. Might have it run just every few seconds in an infinite loop
+    make_improvements();
+    // Attempt to attach the improvement function to dynamically loaded content
+    document.body.addEventListener('DOMNodeInserted', function(event){
+        if(event.target.tagName === 'ARTICLE'){
+            make_improvements();
+        }
+    }, false);
+
+    var loadMoreButton = document.querySelector(".rcom-load-more-button");
+    if (loadMoreButton) {
+        loadMoreButton.onclick = () => setTimeout(make_improvements, 6000); // Adjusts for dynamic content loading
+    }
 };
 
 function make_improvements() {
-  //window.alert("invoked");
-  console.log("invoked");
-  var articles = document.getElementsByTagName('article');
-  
-  for (var i = 0; i < articles.length; i++) {
-    article = articles[i];
-    try {
-		  authorName = authorElement[0].textContent.trim();
-	  } catch (err){
-		  continue;
-	  }
-    authorName = authorElement[0].textContent.trim();
-    console.log(authorName);
-    if (authorName === "Josh Blackman") {
-      //window.alert("found");
-      var replacementElement = document.createElement('h1');
-      var replacementContent = document.createTextNode(replacementText);
-      replacementElement.appendChild(replacementContent);
-      replacementElement.classList.add("rcom-standard-article"); // triggers green line formatting
-  
-      article.replaceWith(replacementElement);
+    console.log("Improve Volokh Extension Invoked");
+    var articles = document.getElementsByTagName('article');
+
+    for (var i = 0; i < articles.length; i++) {
+        var article = articles[i];
+        var authorElements = article.querySelectorAll('.byline.author.vcard a');
+        if (authorElements.length === 0) {
+            continue;
+        }
+
+        var authorName = authorElements[0].textContent.trim();
+        console.log("Author found: " + authorName);
+
+        if (authorName === "Josh Blackman") {
+            console.log("Replacing article by Josh Blackman");
+            var replacementElement = document.createElement('h1');
+            var replacementContent = document.createTextNode(replacementText);
+            replacementElement.appendChild(replacementContent);
+            replacementElement.classList.add("rcom-standard-article"); // Preserves styling
+
+            article.replaceWith(replacementElement);
+        }
     }
-  }
 }
